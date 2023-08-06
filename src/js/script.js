43844fd0a123e3e7fa6eeedee76a0eae556016d0
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-08-06 18:08:35 +0100

    rebuild app.initData() to get data from API server / deleted data.js file

--- src/js/script.js
+++ src/js/script.js
@@ -72,6 +72,11 @@
 		cart: {
 			defaultDeliveryFee: 20,
 		},
+		db: {
+			url: '//localhost:3131',
+			products: 'products',
+			orders: 'orders',
+		},
 	};
 
 	const templates = {
@@ -558,13 +563,34 @@
 	const app = {
 		initData: function () {
 			const thisApp = this;
-			thisApp.data = dataSource;
+			thisApp.data = {};
+			const url = `${settings.db.url}/${settings.db.products}`;
+
+			fetch(url)
+				.then(function (rawResponse) {
+					return rawResponse.json();
+				})
+				.then(function (parsedResponse) {
+					console.log('parsedResponse: ', parsedResponse);
+
+					//save parsedResponse as thisApp.data.products
+					thisApp.data.products = parsedResponse;
+					console.log(thisApp.data.products);
+
+					// execute initMenu method
+					thisApp.initMenu();
+				});
+
+			console.log(JSON.stringify(thisApp.data));
 		},
 		initMenu: function () {
 			const thisApp = this;
 
 			for (let productData in thisApp.data.products) {
-				new Product(productData, thisApp.data.products[productData]);
+				new Product(
+					thisApp.data.products[productData].id,
+					thisApp.data.products[productData]
+				);
 			}
 		},
 		init: function () {
@@ -573,7 +599,7 @@
 			console.log('thisApp:', thisApp);
 
 			thisApp.initData();
-			thisApp.initMenu();
+			// thisApp.initMenu();
 			this.initCart();
 		},
 		initCart() {
