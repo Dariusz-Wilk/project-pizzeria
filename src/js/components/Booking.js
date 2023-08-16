b6c43f2423967952f7b3475013086567e9f9c15b
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-08-16 15:07:06 +0100

    created DatePicker & HourPicker Classes and init their instancies in Booking Class

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,10 +1,14 @@
 import { select, templates } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
 	constructor(element) {
 		this.render(element);
 		this.initWidgets();
+
+		console.log(this);
 	}
 
 	render(element) {
@@ -20,6 +24,12 @@ class Booking {
 		this.dom.hoursAmount = this.dom.wrapper.querySelector(
 			select.booking.hoursAmount
 		);
+		this.dom.datePickerWidget = this.dom.wrapper.querySelector(
+			select.widgets.datePicker.wrapper
+		);
+		this.dom.hourPickerWidget = this.dom.wrapper.querySelector(
+			select.widgets.hourPicker.wrapper
+		);
 	}
 
 	initWidgets() {
@@ -28,6 +38,9 @@ class Booking {
 
 		this.dom.peopleAmount.addEventListener('update', () => {});
 		this.dom.hoursAmount.addEventListener('update', () => {});
+
+		this.datePickerWidget = new DatePicker(this.dom.datePickerWidget);
+		this.hourPickerWidget = new HourPicker(this.dom.hourPickerWidget);
 	}
 }
 
